297d1c77a94671b6164e01592f7a3dccf94c7833
TradingSM <trading.sm2025@gmail.com>
2025-07-15 11:25:08 +0200

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{
+    "name": "Diamond Roulette",
+    "short_name": "Roulette",
+    "start_url": ".",
+    "display": "standalone",
+    "background_color": "#1e3c72",
+    "theme_color": "#2a5298",
+    "icons": [
+        {
+            "src": "icon-192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "icon-512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ]
+}
